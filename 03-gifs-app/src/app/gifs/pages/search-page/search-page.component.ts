42df1f086568c7bc920b8e50e3eec023f99e9e22
CarlosRincon13 <carlos.rincon@indtechcol.com>
2025-10-09 17:35:32 -0500

    [FEATURED]-AddingSearchBar

--- 03-gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
-  templateUrl: './search-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [GifListComponent],
+  templateUrl: './search-page.component.html'
 })
 export default class SearchPageComponent { }
