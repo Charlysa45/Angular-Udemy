de4fb056f1be2e9d18090dc4cb90675a118398a0
CarlosRincon13 <carlos.rincon@indtechcol.com>
2025-10-16 11:27:59 -0500

    [FEATURE]-Section8Completed

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -20,8 +20,18 @@ const loadFromLocalStorage = (): Record<string, Gif[]> => {
 export class GifService {
   private http = inject(HttpClient)
 
-  trendingGifs = signal<Gif[]>([])
-  trendingGifsLoading = signal(true)
+  trendingGifs = signal<Gif[]>([]) // [gif, gif, gif, gif...]
+  trendingGifsLoading = signal(false)
+  private trendingPage = signal(0)
+
+  // [ [gif, gif, gif], [gif, gif, gif], [gif, gif, gif]]
+  trendingGifGroup = computed<Gif[][]>(() => {
+    const groups = []
+    for (let i = 0; i < this.trendingGifs().length; i+=3) {
+      groups.push(this.trendingGifs().slice(i, i + 3))
+    }
+    return groups // [ [gif1, gif2, gif3], [gi4, gi5, gi6] ]
+  })
 
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage())
   searchHistoryKeys = computed(() => Object.keys(this.searchHistory()))
@@ -37,17 +47,22 @@ export class GifService {
   })
 
   loadTrendingGifs() {
+    if (this.trendingGifsLoading()) return;
+
+    this.trendingGifsLoading.set(true)
+
     this.http.get<GiphyResponse>(`${environment.GIPHY_BASE_URL}/gifs/trending`, {
       params: {
         api_key: environment.API_KEY_GIPHY,
-        limit: 20
+        limit: 20,
+        offset: this.trendingPage() * 20
       }
     })
     .subscribe((resp) => {
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data)
-      this.trendingGifs.set(gifs)
+      this.trendingGifs.update((currentGifs) => [...currentGifs, ...gifs])
+      this.trendingPage.update((currentPage) => currentPage + 1)
       this.trendingGifsLoading.set(false)
-      console.log({ gifs });
     })
   }
   
